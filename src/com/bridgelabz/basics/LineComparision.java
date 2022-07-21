2b1f4adb6897f30aa19bec4aad536f1e3a1c2e9f
Nishit <nishitranjan41@gmail.com>
2022-07-21 09:43:27 +0530

    This is welcome of LineComparision

---
+++ src/com/bridgelabz/basics/LineComparision.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.basics;
+
+public class LineComparision {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program!");
+
+    }
+}
